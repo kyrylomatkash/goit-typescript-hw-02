ce3ce6c937208c9bca691a560d873f1d160428e2
Kyrylo <kyrylo.mm@gmail.com>
2024-02-03 21:46:57 +0100

    Completed

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -6,12 +6,12 @@ interface ComponentProps {
   title: string;
 }
 
-class Component<T extends ComponentProps> {
+class Component<T > {
   constructor(public props: T) {
   }
 }
 
-class Page extends Component<{ title: string }> {
+class Page extends Component<ComponentProps> {
   pageInfo() {
     console.log(this.props.title);
   }
